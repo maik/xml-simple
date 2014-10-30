b8ace1dc0c5c8e2ac294c56bc4a19e0328993b37
maik <contact@maik-schmidt.de>
2014-10-30 22:08:39 +0100

    Fixed error message.

--- lib/pre-config.rb
+++ lib/pre-config.rb
@@ -2,5 +2,5 @@ begin
   require 'rexml/document'
   require 'rexml/xpath'
 rescue Exception => ex
-  $stderr.print "\nYou have to install REXML to use XmlConfigFile!!!\n\n"
+  $stderr.print "\nYou have to install REXML to use xml-simple.\n\n"
 end
